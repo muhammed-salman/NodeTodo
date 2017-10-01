a9a4a3c18c6879ebdf5afb638845ad2dbca55989
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2017-10-01 17:06:52 +0530

    Playground fetch and insert data in mongo

---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+const {MongoClient, ObjectID}=require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  // db.collection('Todos').find({_id: new ObjectID('59d0c8cfb1ece212fc255466')}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos',err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos',err);
+  // });
+
+  db.collection('Users').find({name: 'Salman'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs,undefined,2));
+  }, (err) => {
+    console.log('Unable to fetch todos',err);
+  });
+  //db.close();
+});
