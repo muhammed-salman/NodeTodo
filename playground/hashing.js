cd9ce5132df34d1a24078129705a07d0c5e3b89a
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2017-10-19 18:52:33 +0530

    Added Hashing fuctionality

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-	id: 10
-};
+var password = '123abc!';
 
-var token=jwt.sign(data,'1234abcd');
-console.log(token);
+//args: no of rounds, callback
+bcrypt.genSalt(10,(err, salt) => {
+	bcrypt.hash(password,salt,(err,hash)=>{
+		console.log(hash);
+	});
+});
 
-var decoded=jwt.verify(token,'1234abcd');
-console.log('decoded',decoded);
+var hashedPassword = '$2a$10$sd9f8/uxbGBSPPRAaUWNoe5Krg9ZiM3kMpNxoaYE6LuRgoFMjSp32';
 
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+	console.log(res);
+});
+//JWT
+// var data={
+// 	id: 10
+// };
+
+// var token=jwt.sign(data,'1234abcd');
+// console.log(token);
+
+// var decoded=jwt.verify(token,'1234abcd');
+// console.log('decoded',decoded);
+
+//SHA256
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
